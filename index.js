8bf9302abc3154f033bc825cc4e1e3dc51249d1d
Asim14878111 <asim14875111@gmail.com>
2025-03-28 22:19:01 +0500

    Task updated

---
+++ index.js
@@ -0,0 +1,74 @@
+let NAME = [];
+let currentEditIndex = null;
+// Function to add entries 
+
+function addEntry() {
+  const employeename = document.getElementById("employee-name").value;
+  const joiningdate = document.getElementById("joining-date").value;
+  const employeesalary = document.getElementById("employee-salary").value;
+  const employeestatus = document.getElementById("status").value;
+
+  const entry = {
+    employeename,
+    joiningdate,
+    employeesalary,
+    employeestatus,
+  };
+
+
+  const updatebutton = document.getElementById("updatebutton");
+
+
+  if (currentEditIndex !== null) {
+    NAME[currentEditIndex] = entry;
+    currentEditIndex = null;
+    // updatebutton.innerHTML = "Add"
+  } else {
+    NAME.push(entry);
+    updatebutton.innerHTML = "Update/add";
+  }
+  document.getElementById("loginForm").reset();
+  displayEntries();
+}
+
+// Displaying employees record in table
+function displayEntries() {
+  const tbody = document.getElementById("employees-lists");
+  tbody.innerHTML = "";
+
+  NAME.forEach((entry, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+               <td class="row2 pl-4 text-lg font-bold text-gray-600">${entry.employeename}</td>
+               <td class="row2 text-lg font-bold text-gray-600">${entry.joiningdate}</td>
+               <td class="row2 text-lg font-bold text-gray-600">${entry.employeesalary}</td>
+               <td class="row2 text-lg font-bold text-gray-600">${entry.employeestatus}</td>
+               <td class="cursor-pointer text-lg text-blue-500" onclick="deleteItem(${index})">Delete</td>
+               <td class="cursor-pointer text-blue-500 " onclick="editItem(${index})">Edit</td>`;
+    tbody.appendChild(row);
+  });
+}
+
+// Function to delete employee record
+function deleteItem(index) {
+  NAME.splice(index, 1);
+  displayEntries();
+}
+
+// Function to edit employee record
+function editItem(index) {
+  const entry = NAME[index];
+  document.getElementById("employee-name").value = entry.employeename;
+  document.getElementById("joining-date").value = entry.joiningdate;
+  document.getElementById("employee-salary").value = entry.employeesalary;
+  document.getElementById("status").value = entry.employeestatus;
+  currentEditIndex = index;
+}
+
+// Formatting inputs values on submit
+
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  loginForm.reset();
+});
